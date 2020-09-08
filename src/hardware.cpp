408f99325ed6006bb5ef7f93138b47bd01cc7fa8
Dana <dcolletti@sandboxunion.com>
2020-09-08 19:53:18 -0400

    my test bench

--- src/hardware.cpp
+++ src/hardware.cpp
@@ -5,3 +5,12 @@ using namespace Hardware;
 brain Hardware::v5_brain;
 
 controller Hardware::master(controllerType::primary);
+
+motor Hardware::motor1(PORT1);
+motor Hardware::motor2(PORT2, true);
+motor Hardware::motor3(PORT3);
+motor Hardware::motor4(PORT4);
+motor Hardware::motor5(PORT5);
+motor Hardware::motor6(PORT6);
+motor Hardware::motor7(PORT7);
+motor Hardware::motor8(PORT8);
