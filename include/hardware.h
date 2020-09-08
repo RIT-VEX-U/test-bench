9ead2b878b73ca0accb5edaa047755786a62a9a5
Dana <dcolletti@sandboxunion.com>
2020-09-08 19:47:16 -0400

    blank copy

--- include/hardware.h
+++ include/hardware.h
@@ -9,14 +9,6 @@ namespace Hardware {
   extern brain v5_brain;
   extern controller master;
 
-  extern motor motor1;
-  extern motor motor2;
-  extern motor motor3;
-  extern motor motor4;
-  extern motor motor5;
-  extern motor motor6;
-  extern motor motor7;
-  extern motor motor8;
 }
 
 #endif
