8d481634827f8ab94f6a3d95d11fa12ddd34f631
Victor Rabinovich <53245742+Rabinvic@users.noreply.github.com>
2022-09-04 14:37:04 -0400

    Motor spin
    Made motor spin while button is pressed

--- src/hardware.cpp
+++ src/hardware.cpp
@@ -5,3 +5,7 @@ using namespace Hardware;
 brain Hardware::v5_brain;
 
 controller Hardware::master(controllerType::primary);
+
+motor Hardware::m1(0), Hardware::m2(1), Hardware::m3(2), Hardware::m4(3), Hardware::m5(4), Hardware::m6(5), Hardware::m7(6), Hardware::m8(7);
+
+button Hardware
