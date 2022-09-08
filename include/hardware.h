d60757b04cbd7ba8e5971e6959932104db6ed7da
Victor Rabinovich <53245742+Rabinvic@users.noreply.github.com>
2022-09-08 19:09:15 -0400

    Test Bench Done
    Added motor control and speed control

--- include/hardware.h
+++ include/hardware.h
@@ -9,7 +9,7 @@ namespace Hardware {
   extern brain v5_brain;
   extern controller master;
   extern motor m1,m2,m3,m4,m5,m6,m7,m8;
-  extern button m1b,m2b,m3b,m4b,m5b,m6b,m7b,m8b;
+  
 
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,76 @@
 
 #include "vex.h"
 #include "hardware.h"
+#include "hardware.cpp"
 
 using namespace vex;
+
+double speed=4.0;
+int PCT=75;
 
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
+  while(true){
+    master.ButtonR2.pressed(speedUp);
+    master.ButtonL2.pressed(speedDown);
 
+    if(master.ButtonA.pressing()){
+      m1.spin(forward,speed,pct);
+    }else{
+      m1.stop();
+    }
+
+    if(master.ButtonB.pressing()){
+      m2.spin(forward,speed,pct);
+    }else{
+      m2.stop();
+    }
+
+    if(master.ButtonX.pressing()){
+      m3.spin(forward,speed,pct);
+    }else{
+      m3.stop();
+    }
+
+    if(master.ButtonY.pressing()){
+      m4.spin(forward,speed,pct);
+    }else{
+      m4.stop();
+    }
+
+    if(master.ButtonRight.pressing()){
+      m5.spin(forward,speed,pct);
+    }else{
+      m5.stop();
+    }
+
+    if(master.ButtonUp.pressing()){
+      m6.spin(forward,speed,pct);
+    }else{
+      m6.stop();
+    }
+
+    if(master.ButtonLeft.pressing()){
+      m7.spin(forward,speed,pct);
+    }else{
+      m7.stop();
+    }
+
+    if(master.ButtonDown.pressing()){
+      m8.spin(forward,speed,pct);
+    }else{
+      m8.stop();
+    }
+
+  }
 }
+void speedUp(){
+  if(speed<=80)
+    speed*=1.25;
+  }
+
+void speedDown(){
+  if(speed>=.25)
+    speed*=.75;
+  }
