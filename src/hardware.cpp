5f5c2a634178ca7a64c8c445d8eb818dd3f9b8b7
Anishya Thinesh <amt2622@rit.edu>
2023-09-03 14:33:47 -0400

    first try

--- src/hardware.cpp
+++ src/hardware.cpp
@@ -5,3 +5,13 @@ using namespace Hardware;
 brain Hardware::v5_brain;
 
 controller Hardware::master(controllerType::primary);
+
+button& vex::controller::ButtonA = buttonA;
+button& vex::controller::ButtonB = buttonB;
+button& vex::controller::ButtonX = buttonX;
+
+vex::motor motor1 = motor(PORT1);
+
+vex::motor motor2 = motor(PORT2);
+
+vex::motor motor3 = motor(PORT3);
--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,21 @@
 
 using namespace vex;
 
+// double const VELOCITY = 1.0;
+vex directionType dir = fwd;
+
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
+
+  while (true) {
+    if (buttonA.pressing()) {
+      motor1.spin(dir);
+    } else if (buttonB.pressing()) {
+      motor2.spin(dir);
+    } else if (buttonX.pressing()) {
+      motor3.spin(dir);
+    }
+  }
 
 }
