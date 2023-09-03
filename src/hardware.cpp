219766e33b0261ae8d9cd732745744f0324534d5
Kelvin0K <havoc2830@gmail.com>
2023-09-03 15:16:16 -0400

    basic

--- src/hardware.cpp
+++ src/hardware.cpp
@@ -8,9 +8,9 @@ controller Hardware::master(controllerType::primary);
 
 vex::motor motor1 = vex::motor(PORT1);
 vex::motor motor2 = vex::motor(PORT2);
-vex::motor motor1 = vex::motor(PORT3);
-vex::motor motor2 = vex::motor(PORT4);
-vex::motor motor1 = vex::motor(PORT5);
-vex::motor motor2 = vex::motor(PORT6);
-vex::motor motor1 = vex::motor(PORT7);
-vex::motor motor2 = vex::motor(PORT8);
+vex::motor motor3 = vex::motor(PORT3);
+vex::motor motor4 = vex::motor(PORT4);
+vex::motor motor5 = vex::motor(PORT5);
+vex::motor motor6 = vex::motor(PORT6);
+vex::motor motor7 = vex::motor(PORT7);
+vex::motor motor8 = vex::motor(PORT8);
--- src/main.cpp
+++ src/main.cpp
@@ -19,24 +19,23 @@ int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
 
-  while (1)
-  {
-    if (Hardware::master.ButtonA.pressing())
-      motor1.spin(forward);
-    if (Hardware::master.ButtonB.pressing())
-      motor2.spin(forward);
-    if (Hardware::master.ButtonX.pressing())
-      motor3.spin(forward);
-    if (Hardware::master.ButtonY.pressing())
-      motor4.spin(forward);
-    if (Hardware::master.ButtonRight.pressing())
-      motor5.spin(forward);
-    if (Hardware::master.ButtonLeft.pressing())
-      motor6.spin(forward);
-    if (Hardware::master.ButtonUp.pressing())
-      motor7.spin(forward);
-    if (Hardware::master.ButtonDown.pressing())
-      motor8.spin(forward);
-  }
+  Hardware::master.ButtonA.pressed([](){motor1.spin(forward);});
+  Hardware::master.ButtonA.released([](){motor1.stop();});
+  Hardware::master.ButtonB.pressed([](){motor2.spin(forward);});
+  Hardware::master.ButtonB.released([](){motor2.stop();});
+  Hardware::master.ButtonX.pressed([](){motor3.spin(forward);});
+  Hardware::master.ButtonX.released([](){motor3.stop();});
+  Hardware::master.ButtonY.pressed([](){motor4.spin(forward);});
+  Hardware::master.ButtonY.released([](){motor4.stop();});
+  Hardware::master.ButtonRight.pressed([](){motor5.spin(forward);});
+  Hardware::master.ButtonRight.released([](){motor5.stop();});
+  Hardware::master.ButtonUp.pressed([](){motor6.spin(forward);});
+  Hardware::master.ButtonUp.released([](){motor6.stop();});
+  Hardware::master.ButtonLeft.pressed([](){motor7.spin(forward);});
+  Hardware::master.ButtonLeft.released([](){motor7.stop();});
+  Hardware::master.ButtonDown.pressed([](){motor8.spin(forward);});
+  Hardware::master.ButtonDown.released([](){motor8.stop();});
+
+  // Hardware::master.ButtonR2.pressed([](){motor1.setVelocity();});
 
 }
